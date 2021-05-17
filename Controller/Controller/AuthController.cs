6db1726ce15e7bee4936b66a25d9ff5d2bd9ab50
Adam Coulter <ads@me.com>
2021-05-17 16:58:33 +1000

    check authHeader Value

--- Controller/Controller/AuthController.cs
+++ Controller/Controller/AuthController.cs
@@ -8,16 +8,12 @@ namespace PurpleDepot.Controller
 	{
 		public static void Authenticate(this HttpRequestMessage request)
 		{
-			var token = Environment.GetEnvironmentVariable("PURPLEDEPOT_AUTHTOKEN");
-			if (token is null)
-				throw new HttpResponseException(request, HttpStatusCode.InternalServerError, "Authentication is not configured.");
 			var authHeader = request.Headers.Authorization;
 			if (authHeader is null)
 				throw new HttpResponseException(request, HttpStatusCode.Unauthorized, "Authorization header hasn't been provided, and is required.");
 			if(authHeader.Scheme != "Bearer")
 				throw new HttpResponseException(request, HttpStatusCode.Unauthorized, $"Incorrected authorisation type provided, expected 'bearer' got '{authHeader.Scheme}'");
-			if(authHeader.Parameter != token)
-				throw new HttpResponseException(request, HttpStatusCode.Unauthorized, "Bearer token invalid.");
+			throw new HttpResponseException(request, HttpStatusCode.Unauthorized, authHeader.Parameter);
 		}
 	}
 }
