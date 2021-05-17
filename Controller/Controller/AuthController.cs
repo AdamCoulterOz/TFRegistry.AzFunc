824cdd95dbce946049e147e772639e158854a02f
Adam Coulter <ads@me.com>
2021-05-17 17:25:27 +1000

    check roles in JWT

--- Controller/Controller/AuthController.cs
+++ Controller/Controller/AuthController.cs
@@ -1,6 +1,7 @@
-using System;
 using System.Net;
 using System.Net.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 
 namespace PurpleDepot.Controller
 {
@@ -11,9 +12,22 @@ namespace PurpleDepot.Controller
 			var authHeader = request.Headers.Authorization;
 			if (authHeader is null)
 				throw new HttpResponseException(request, HttpStatusCode.Unauthorized, "Authorization header hasn't been provided, and is required.");
-			if(authHeader.Scheme != "Bearer")
+			if (authHeader.Scheme != "Bearer")
 				throw new HttpResponseException(request, HttpStatusCode.Unauthorized, $"Incorrected authorisation type provided, expected 'bearer' got '{authHeader.Scheme}'");
-			throw new HttpResponseException(request, HttpStatusCode.Unauthorized, authHeader.Parameter);
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var rawToken = authHeader.Parameter;
+
+			var readableToken = tokenHandler.CanReadToken(rawToken);
+
+			if (readableToken != true)
+				throw new HttpResponseException(request, HttpStatusCode.Unauthorized, "The token doesn't seem to be in a proper JWT format.");
+
+			var token = tokenHandler.ReadJwtToken(rawToken);
+			var roles = token.Claims.Where(claim => claim.Type == "roles").First().Value;
+
+			if(roles.Contains("Terraform.Module.Contributor"))
+				throw new HttpResponseException(request, HttpStatusCode.Unauthorized, $"The token doesn't doesn't contain the module contributor role, found roles: {roles}");
 		}
 	}
 }
