7e0f1c9f1df3bd3924c504e2039b9d282077e5da
Adam Coulter <adam.coulter@outlook.com>
2022-03-08 20:45:55 +1100

    remove prefix

--- Providers/Azure/Test/runLoginTest.ps1
+++ Providers/Azure/Test/runLoginTest.ps1
@@ -1,11 +1,11 @@
 Install-Module -Name Az.Accounts -Scope CurrentUser -Repository PSGallery -Force
 
-$secureClientSecret = $env:test_clientSecret | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ($env:test_clientId, $secureClientSecret)
+$secureClientSecret = $env:clientSecret | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($env:clientId, $secureClientSecret)
 
-Connect-AzAccount -ServicePrincipal -Credential $credential -Tenant $env:test_tenantId
+Connect-AzAccount -ServicePrincipal -Credential $credential -Tenant $env:tenantId
 
-$token = (Get-AzAccessToken -ResourceUrl $env:test_registryAuth -TenantId $tenantId).Token
+$token = (Get-AzAccessToken -ResourceUrl $env:registryAuth -TenantId $tenantId).Token
 
 $secureToken = $token | ConvertTo-SecureString -AsPlainText -Force
-Invoke-RestMethod -Uri "$env:test_registryUrl/v1/modules/adam/server/azure" -Method GET -Authentication OAuth -Token $secureToken
+Invoke-RestMethod -Uri "$env:registryUrl/v1/modules/adam/server/azure" -Method GET -Authentication OAuth -Token $secureToken
