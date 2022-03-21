9ba208a45a68b8963dc39027ccea28c411412b3a
Adam Coulter <adam.coulter@outlook.com>
2022-03-21 17:07:22 +1100

    use minimum TLS 1.2

--- Providers/Azure/Module/storage.tf
+++ Providers/Azure/Module/storage.tf
@@ -5,6 +5,7 @@ resource "azurerm_storage_account" "repo" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   allow_blob_public_access = true
+  min_tls_version          = "TLS1_2"
 }
 
 resource "azurerm_role_assignment" "controller_storage_access" {
@@ -18,4 +19,3 @@ resource "azurerm_storage_container" "registry" {
   storage_account_name  = azurerm_storage_account.repo.name
   container_access_type = "blob"
 }
-
